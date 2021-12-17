222af30d3e7e912d016904d92ee53bc85e3c6c74
ramsjenu <vramschennai@gmail.com>
2021-12-17 16:22:17 +0000

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -7,7 +7,7 @@ data "aws_availability_zones" "available" {}
 
 
 locals {
-  cluster_name = "matts-eks-${random_string.suffix.result}"
+  cluster_name = "vrams-eks-${random_string.suffix.result}"
 }
 
 resource "random_string" "suffix" {
@@ -19,7 +19,7 @@ module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "3.2.0"
 
-  name = "matts-vpc"
+  name = "vrams-vpc"
   cidr = "10.0.0.0/16"
   azs                  = data.aws_availability_zones.available.names
   private_subnets      = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
